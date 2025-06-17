5795b9178229fb9823053185a140a327f891a123
Amir Tuval <amirtu@tabnine.com>
2025-06-17 12:21:06 +0300

    add requests logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 console.log("Starting server...");
 
@@ -12,6 +13,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(morgan('combined'));
 
 // Database connection
 const pool = new Pool({
